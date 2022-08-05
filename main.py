307b985ea23b1314abb30a0ecd6372d291fa1a66
2gaudy <agrant9946@gmail.com>
2022-08-05 11:22:50 -0400

    Learned new features with Jinja that allow for working with python code in HTML

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 import datetime as dt
 import requests
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -14,11 +15,12 @@ def home():
     random_number = random.randint(1, 10)
     return render_template("index.html", num=random_number, CURRENT_YEAR=year, CREATOR=name)
 
+
 @app.route("/guess/<string:name>")
 def guess(name):
     params_age = {
-        "name":name,
-        "country_id":"US"
+        "name": name,
+        "country_id": "US"
     }
 
     params_gender = {
@@ -33,9 +35,14 @@ def guess(name):
     gender_response = requests.get(url="https://api.genderize.io", params=params_gender)
     gender = gender_response.json()
 
-
     return render_template("guess.html", name=name, gender=gender["gender"], age=age["age"])
 
+@app.route("/blog/<num>")
+def blog(num):
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
